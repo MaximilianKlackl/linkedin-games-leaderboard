812fd9b52129a3aef3faaed8448d4e4948d5fceb
Max <klacklmaximilian@gmail.com>
2025-06-02 18:40:07 +0200

    fix

--- api/leaderboard.ts
+++ api/leaderboard.ts
@@ -1,4 +1,9 @@
-import supabase from './supabase-client'
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 
 type GAME = 'tango' | 'queens' | 'zip';
 
