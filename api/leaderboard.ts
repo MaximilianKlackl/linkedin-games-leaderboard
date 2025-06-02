f598cada02b517a0d56cd3e1d1e8ad3159489331
Max <klacklmaximilian@gmail.com>
2025-06-02 18:50:36 +0200

    fix

--- api/leaderboard.ts
+++ api/leaderboard.ts
@@ -2,8 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 type GAME = 'tango' | 'queens' | 'zip';
 
-async function GET(request: Request) {
-
+export async function GET(request: Request) {
     const supabase = createClient(
         process.env.SUPABASE_URL!,
         process.env.SUPABASE_ANON_KEY!
