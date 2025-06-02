db0274d5228caad3335242555fd50b297a25b437
Max <klacklmaximilian@gmail.com>
2025-06-02 18:18:37 +0200

    supabase test

--- api/leaderboard.ts
+++ api/leaderboard.ts
@@ -1,3 +1,15 @@
-export function GET(request: Request) {
+import supabase from "./utils/supabase";
+import createClient from "./utils/supabase";
+
+type GAME = 'tango' | 'queens' | 'zip';
+
+async function GET(request: Request) {
+
+    const {data, error} = await supabase.from('result').select()
+
+    return new Response(JSON.stringify(data));
+}
+
+export function POST(request: Request) {
     return new Response('Hello From the Serverless Function');
 }
