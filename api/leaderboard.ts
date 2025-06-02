22a5f1f08b6571514fa30dd278fd6c2515f7bba0
Max <klacklmaximilian@gmail.com>
2025-06-02 18:36:58 +0200

    test

--- api/leaderboard.ts
+++ api/leaderboard.ts
@@ -1,15 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import supabase from './supabase-client'
 
 type GAME = 'tango' | 'queens' | 'zip';
 
 async function GET(request: Request) {
 
-    const client = createClient(
-        process.env.SUPABASE_URL,
-        process.env.SUPABASE_ANON_KEY
-    );
-
-    const {data, error} = await client.from('result').select()
+    const {data, error} = await supabase.from('result').select()
 
     return new Response(JSON.stringify(data));
 }
